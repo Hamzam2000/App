46cf1fd2db32c3c370fcb502a8e7e87abdfadc55
Hamza <hamza.games2000@hotmail.com>
2018-09-04 13:53:23 +0200

    ssss

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-
-
-
-
-
-
-
-
-
-
-
-?>
+$mollie = new \Mollie\Api\MollieApiClient();
+$mollie->setApiKey("");
+$payment = $mollie->payments->create([
+    "amount" => [
+        "currency" => "EUR",
+        "value" => "10.00" // You must send the correct number of decimals, thus we enforce the use of strings
+    ],
+    "description" => "My first payment",
+    "redirectUrl" => "https://webshop.example.org/order/12345/",
+    "webhookUrl" => "https://webshop.example.org/payments/webhook/",
+    "metadata" => [
+        "order_id" => "12345",
+    ],
+]);
